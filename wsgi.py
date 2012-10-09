d06d7ebe33e805a17f2ae85e43211f9a6f524856
Taro Sato <okomestudio@gmail.com>
2012-10-09 15:23:19 -0700

    another way for wsgi?

--- wsgi.py
+++ wsgi.py
@@ -3,12 +3,17 @@
 #
 # For example, to load the app from your "production.ini" file in the same
 # directory:
-import os.path
+#import os.path
+import os
 from pyramid.scripts.pserve import cherrypy_server_runner
-from pyramid.paster import get_app
+f#rom pyramid.paster import get_app
+from paste.deploy import loadapp
 
-application = get_app(os.path.join(os.path.dirname(__file__),
-                                   'production.ini'))
+#application = get_app(os.path.join(os.path.dirname(__file__),
+#                                   'production.ini'))
+
+current_dir = os.getcwd()
+application = loadapp('config:production.ini', relative_to=current_dir)
 
 if __name__ == "__main__":
     cherrypy_server_runner(application, host="0.0.0.0")
