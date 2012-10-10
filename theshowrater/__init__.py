d715c67a4f0eb8176c48177328194141e837bcaa
Taro Sato <okomestudio@gmail.com>
2012-10-10 00:41:08 -0700

    try to use mysql on dotcloud

--- theshowrater/__init__.py
+++ theshowrater/__init__.py
@@ -1,14 +1,35 @@
 import os
+import json
 from pyramid.config import Configurator
+
+
+def dbsetting():
+    # this is for local
+    #db = {'db': 'lahman591',
+    #      'read_default_file': '/'.join([os.environ["HOME"],
+    #                                     ".my.cnf"]) }
+    DOTCLOUD_ENV_FILE = "/home/dotcloud/environment.json"
+    if os.path.exists(DOTCLOUD_ENV_FILE):
+        with open(DOTCLOUD_ENV_FILE) as f:
+            env = json.load(f)
+            host = env['DOTCLOUD_DB_MYSQL_HOST']
+            user = env['DOTCLOUD_DB_MYSQL_LOGIN']
+            passwd = env['DOTCLOUD_DB_MYSQL_PASSWORD']
+            port = env['DOTCLOUD_DB_MYSQL_PORT']
+
+        db = {'db': 'lahman591',
+              'host': host,
+              'user': user,
+              'passwd': passwd,
+              'port': port}
+
+    return db
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    settings['db'] = {'db': 'lahman591',
-                      'read_default_file': '/'.join([os.environ["HOME"],
-                                                     ".my.cnf"]) }
-
+    settings['db'] = dbsetting()
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
